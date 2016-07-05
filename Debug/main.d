3e060ab54c77eb760349b417d2bd60ff038f3997
Joe Altmaier <joe.altmaier@gmail.com>
2016-07-05 16:15:50 -0500

    Add project files

---
+++ Debug/main.d
@@ -0,0 +1,23 @@
+# FIXED
+
+main.obj: ../main.c
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.2/include/stdint.h
+main.obj: C:/ti/pru-software-support-package/include/am437x/pru_cfg.h
+main.obj: C:/ti/pru-software-support-package/include/am437x/pru_intc.h
+main.obj: C:/ti/pru-software-support-package/include/am437x/pru_iep.h
+main.obj: ../rsc_table_pru.h
+main.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.2/include/stddef.h
+main.obj: C:/ti/pru-software-support-package/include/rsc_types.h
+main.obj: C:/ti/pru-software-support-package/include/pru_types.h
+main.obj: C:/ti/pru-software-support-package/include/pru_virtio_ids.h
+
+../main.c: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.2/include/stdint.h: 
+C:/ti/pru-software-support-package/include/am437x/pru_cfg.h: 
+C:/ti/pru-software-support-package/include/am437x/pru_intc.h: 
+C:/ti/pru-software-support-package/include/am437x/pru_iep.h: 
+../rsc_table_pru.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-pru_2.1.2/include/stddef.h: 
+C:/ti/pru-software-support-package/include/rsc_types.h: 
+C:/ti/pru-software-support-package/include/pru_types.h: 
+C:/ti/pru-software-support-package/include/pru_virtio_ids.h: 
